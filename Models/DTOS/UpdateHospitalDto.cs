04cc840e00be7a08bcf80c16ed5b5ad3132004e9
unknown <ariana.pita@ucb.edu.bo>
2025-10-26 17:49:52 -0400

    feat: Implementation of Update Hospital Dto

--- Models/DTOS/UpdateHospitalDto.cs
+++ Models/DTOS/UpdateHospitalDto.cs
@@ -1,6 +1,16 @@
-﻿namespace Security.Models.DTOS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Security.Models.DTOS
 {
-    public class UpdateHospitalDto
+    public record UpdateHospitalDto
     {
+        public Guid Id { get; init; } 
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public int? Type { get; set; } 
     }
 }
+
+
+
+
