58b85ea56fad6021c1e59421a6158ef9fa209390
unknown <ariana.pita@ucb.edu.bo>
2025-10-26 17:59:37 -0400

    feat: implement hospital update logic

--- Services/HospitalService.cs
+++ Services/HospitalService.cs
@@ -33,5 +33,20 @@ namespace Security.Services
         {
             return await _repo.GetOne(id);
         }
+
+        public async Task<Hospital?> UpdateHospital(UpdateHospitalDto dto)
+        {
+            var hospital = await _repo.GetOne(dto.Id);
+            if (hospital == null) return null;
+
+            
+            if (dto.Name != null) hospital.Name = dto.Name;
+            if (dto.Address != null) hospital.Address = dto.Address;
+            if (dto.Type.HasValue) hospital.Type = dto.Type.Value;
+
+            
+            await _repo.Update(hospital);
+            return hospital;
+        }
     }
 }
